8b176b67dde4678b6b6d1da7411b498992687333
Arl44 <117769424+ArlTrt@users.noreply.github.com>
2025-06-20 18:48:24 +0200

    particles collisions with segment (forgot to commit)

--- src/main.cpp
+++ src/main.cpp
@@ -34,8 +34,8 @@ struct Particle
         acceleration = glm::vec2(0.0f);
 
         age = 0.0f;
-        lifetime = utils::rand(2.0f, 5.0f);
-        initial_radius = 0.02f * lifetime;
+        lifetime = utils::rand(20.0f, 50.0f);
+        initial_radius = 0.02f * (lifetime * 0.1);
 
         startColor = glm::vec4(
         utils::rand(0.5f, 1.0f),
@@ -108,7 +108,57 @@ struct Particle
     {
         return age >= lifetime;
     }
+
+    void bounce(const glm::vec2& collision_point, const glm::vec2& normal, float elasticity = 0.7f) {
+
+        float distance_through_wall = glm::length(position - collision_point);
+
+        velocity = glm::reflect(velocity, normal) * elasticity;
+
+        position = collision_point + glm::normalize(velocity) * distance_through_wall;
+    }
 };
+
+std::optional<glm::vec2> intersection(glm::vec2 origin1, glm::vec2 end1, glm::vec2 origin2, glm::vec2 end2)
+{
+    glm::vec2 d1 = end1 - origin1;
+    glm::vec2 d2 = end2 - origin2;
+
+    glm::vec2 delta_origin = origin2 - origin1;
+
+    glm::mat2 M(d1, -d2);
+
+    float det = glm::determinant(M);
+
+    const float epsilon = 1e-6f;
+    if (std::abs(det) < epsilon) {
+        return std::nullopt; 
+    }
+
+    glm::vec2 params = glm::inverse(M) * delta_origin;
+    float t1 = params.x; 
+    float t2 = params.y;
+
+    if (t1 >= 0.0f && t1 <= 1.0f && t2 >= 0.0f && t2 <= 1.0f) {
+        return origin1 + t1 * d1; 
+    }
+
+    return std::nullopt;
+}
+
+void check_and_draw_intersection(glm::vec2 seg1_start, glm::vec2 seg1_end, glm::vec2 seg2_start, glm::vec2 seg2_end)
+{
+    auto intersection_point = intersection(seg1_start, seg1_end, seg2_start, seg2_end);
+    
+    if (intersection_point) {
+        utils::draw_disk(*intersection_point, 0.05f, glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
+    }
+}
+
+glm::vec2 getSegmentNormal(glm::vec2 start, glm::vec2 end) {
+    glm::vec2 direction = glm::normalize(end - start);
+    return glm::vec2(-direction.y, direction.x);
+}
 
 int main()
 {
@@ -119,6 +169,16 @@ int main()
 
     // TODO: create an array of particles
     std::vector<Particle> particles(100);
+
+    //segment 1
+    glm::vec2 seg_start = glm::vec2(-1.f, 0.0f);
+    glm::vec2 seg_end   = glm::vec2( 1.f, 0.0f);
+
+    //segment 2
+    glm::vec2 seg2_start = glm::vec2(0.0f, -0.5f); 
+
+    float thickness = 0.01f;
+    glm::vec4 color = glm::vec4(1, 1, 1, 1);
     
 
     while (gl::window_is_open())
@@ -138,7 +198,18 @@ int main()
         // TODO render particles
         for (auto& particle : particles)
         {
+            glm::vec2 prev_pos = particle.position;
+
             particle.update(dt);
+
+            if (auto intersect = intersection(seg_start, seg_end, prev_pos, particle.position)) {
+                
+                glm::vec2 normal = getSegmentNormal(seg_start, seg_end);
+
+                particle.bounce(*intersect, normal);
+
+                particle.position = *intersect + normal * 0.001f;
+            }
 
             utils::draw_disk(
                 particle.position,  // Position 
@@ -146,6 +217,15 @@ int main()
                 //glm::vec4(1.f, 1.f, 1.f, 1.f),   // Color
                 particle.getCurrentColor()
             );
+
+            check_and_draw_intersection(seg_start, seg_end, prev_pos, particle.position);
         };
+
+        utils::draw_line(seg_start, seg_end, thickness, color);
+        utils::draw_line(seg2_start, gl::mouse_position(), thickness, color);
+        
+        check_and_draw_intersection(seg_start, seg_end, seg2_start, gl::mouse_position());
+        
+        
     }
 }
